4e62bbf22e27460b5c6e835e101293fa7ba880c3
Valeriesantos <valnika96@gmail.com>
2016-10-04 21:33:25 +0200

    dj

--- ValerieSantos.java
+++ ValerieSantos.java
@@ -4,5 +4,6 @@ public class ValerieSantos{
 		System.out.println("Valerie Santos Villar");
 		System.out.println("18 años");
 		System.out.println("León");
+		System.out.println("Soy DJ, desde los 15 años. Especialmente en la rama del tech house y techno");
 		}
 }
